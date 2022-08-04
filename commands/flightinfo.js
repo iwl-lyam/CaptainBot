5c73a971cd61db93e897f9b8ad42e97439a9aa7c
lyammosnier1 <64089164+iwl-lyam@users.noreply.github.com>
2022-08-04 02:08:31 +0000

    Stuff

--- commands/flightinfo.js
+++ commands/flightinfo.js
@@ -1,7 +1,6 @@
-const { SlashCommandBuilder, AttachmentBuilder, EmbedBuilder } = require('discord.js')
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js')
 const fetch = require('node-fetch')
 const {apikey} = require("../config.json")
-const maps = require("staticmaps")
 
 module.exports = {
 	name: "flightinfo",
@@ -18,12 +17,13 @@ module.exports = {
 		let icao = !!interaction.options.getString('icao')
 		let reg = !!interaction.options.getString('iata')
 		let link = `https://airlabs.co/api/v9/flights?api_key=${apikey}&${icao ? "flight_icao="+id : "flight_iata="+id}`
+		let link2 = `https://airlabs.co/api/v9/flight?api_key=${apikey}&${icao ? "flight_icao="+id : "flight_iata="+id}`
+
 console.log(link)
 		fetch(link).then(r => r.json()).then(async res => {
-			if (res.response[1]) return interaction.editReply("There are multiple flights with this ICAO/IATA!")
-			if (!res.response[0]) return interaction.editReply("There are no flights with this ICAO/IATA!")
 			let flight = res.response[0]
-
+			if (!flight) return interaction.editReply("There are no flights with this ICAO/IATA!")
+			
 			interaction.editReply({
 				embeds: [ new EmbedBuilder()
 						.setTitle("Current flight information for " + id)
@@ -34,8 +34,7 @@ console.log(link)
 							{ name: "Altitude", value: Math.floor(flight.alt * 3.28084) + " ft", inline: true },
 							{ name: "Speed", value: Math.floor(flight.speed / 1.852) + " kt", inline: true },
 							{ name: "Heading", value: flight.dir + "°", inline: true },
-							{ name: "Latitude", value: flight.lat.toString(), inline: true },
-							{ name: "Longitude", value: flight.lng.toString(), inline: true },
+							{ name: "Co-ordinates", value: `${flight.lat},${flight.lng}`, inline: true },
 							{ name: "Flight Number", value: flight.flight_number.toString(), inline: true },
 							{ name: "Flight ICAO", value: flight.flight_icao, inline: true },
 							{ name: "Flight IATA", value: flight.flight_iata, inline: true },
@@ -46,9 +45,7 @@ console.log(link)
 							{ name: "Status", value: `${flight.status}`, inline: true },
 							{ name: "Last updated", value: `<t:${flight.updated}>`, inline: true }
 						)
-						.setImage(`attachment://${id}.png`)
-				],
-				files: [mapimg]
+				]
 			})
 		})
 		
