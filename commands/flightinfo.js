021471c3fb3eed595a6b9c6d881ff2ec8223b65d
lyammosnier1 <64089164+iwl-lyam@users.noreply.github.com>
2022-08-06 11:06:43 +0000

    Warnings

--- commands/flightinfo.js
+++ commands/flightinfo.js
@@ -1,6 +1,6 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js')
 const fetch = require('node-fetch')
-const {apikey} = require("../config.json")
+const { apikey } = require("../config.json")
 
 module.exports = {
 	name: "flightinfo",
@@ -16,38 +16,38 @@ module.exports = {
 		await interaction.deferReply()
 		let icao = !!interaction.options.getString('icao')
 		let reg = !!interaction.options.getString('iata')
-		let link = `https://airlabs.co/api/v9/flights?api_key=${apikey}&${icao ? "flight_icao="+id : "flight_iata="+id}`
+		let link = `https://airlabs.co/api/v9/flights?api_key=${apikey}&${icao ? "flight_icao=" + id : "flight_iata=" + id}`
 
 		fetch(link).then(r => r.json()).then(async res => {
 			let flight = res.response[0]
-			if (!flight) return interaction.editReply("There are no flights with this ICAO/IATA!")
-			
+			if (!flight) return interaction.editReply("There are no flights with this ICAO/IATA! (or, the service we are using doesn't cover this flight)")
+
 			interaction.editReply({
-				embeds: [ new EmbedBuilder()
-						.setTitle("Current flight information for " + id)
-						.setFields(
-							{ name: "Country", value: flight.flag, inline: true },
-							{ name: "Registration", value: flight.reg_number, inline: true },
-							{ name: "IACO 24-bit hex", value: flight.hex, inline: true },
-							{ name: "Altitude", value: Math.floor(flight.alt * 3.28084) + " ft", inline: true },
-							{ name: "Speed", value: Math.floor(flight.speed / 1.852) + " kt", inline: true },
-							{ name: "Heading", value: flight.dir + "°", inline: true },
-							{ name: "Co-ordinates", value: `${flight.lat},${flight.lng}`, inline: true },
-							{ name: "Flight Number", value: flight.flight_number.toString(), inline: true },
-							{ name: "Flight ICAO", value: flight.flight_icao, inline: true },
-							{ name: "Flight IATA", value: flight.flight_iata, inline: true },
-							{ name: "Origin", value: `${flight.dep_iata} (${flight.dep_icao})`, inline: true },
-							{ name: "Destination", value: `${flight.arr_iata} (${flight.arr_icao})`, inline: true },
-							{ name: "Airline", value: `${flight.airline_icao} (${flight.airline_iata})`, inline: true },
-							{ name: "Aircraft", value: `${flight.aircraft_icao}`, inline: true },
-							{ name: "Status", value: `${flight.status}`, inline: true },
-							{ name: "Last updated", value: `<t:${flight.updated}>`, inline: true }
-						)
-						.setColor("#AA55AA")
+				embeds: [new EmbedBuilder()
+					.setTitle("Current flight information for " + id)
+					.setFields(
+						{ name: "Country", value: flight.flag, inline: true },
+						{ name: "Registration", value: flight.reg_number, inline: true },
+						{ name: "IACO 24-bit hex", value: flight.hex, inline: true },
+						{ name: "Altitude", value: Math.floor(flight.alt * 3.28084) + " ft", inline: true },
+						{ name: "Speed", value: Math.floor(flight.speed / 1.852) + " kt", inline: true },
+						{ name: "Heading", value: flight.dir + "°", inline: true },
+						{ name: "Co-ordinates", value: `${flight.lat},${flight.lng}`, inline: true },
+						{ name: "Flight Number", value: flight.flight_number.toString(), inline: true },
+						{ name: "Flight ICAO", value: flight.flight_icao, inline: true },
+						{ name: "Flight IATA", value: flight.flight_iata, inline: true },
+						{ name: "Origin", value: `${flight.dep_iata} (${flight.dep_icao})`, inline: true },
+						{ name: "Destination", value: `${flight.arr_iata} (${flight.arr_icao})`, inline: true },
+						{ name: "Airline", value: `${flight.airline_icao} (${flight.airline_iata})`, inline: true },
+						{ name: "Aircraft", value: `${flight.aircraft_icao}`, inline: true },
+						{ name: "Status", value: `${flight.status}`, inline: true },
+						{ name: "Last updated", value: `<t:${flight.updated}>`, inline: true }
+					)
+					.setColor("#AA55AA")
 
 				]
 			})
 		})
-		
+
 	}
 }
