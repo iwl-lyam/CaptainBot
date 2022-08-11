0ca9125c1895bd01059022b78622190c462621ae
lyammosnier1 <64089164+iwl-lyam@users.noreply.github.com>
2022-08-11 20:30:47 +0000

    Gateway connection

--- index.js
+++ index.js
@@ -1,36 +1,94 @@
-const fs = require('node:fs');
-const path = require('node:path');
-const { Client, Collection, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
+const {WebSocket} = require("ws")
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const GATEWAY = "wss://gateway.discord.gg/?v=10&encoding=json"
 
-client.commands = new Collection();
-const commandsPath = path.join(__dirname, 'commands');
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+let gateway = new WebSocket(GATEWAY)
 
-for (const file of commandFiles) {
-	const filePath = path.join(commandsPath, file);
-	const command = require(filePath);
-	client.commands.set(command.name, command);
-}
+gateway.on('close', function error(data) {
+	console.log(data)
+})
 
-client.once('ready', () => {
-	console.log('Ready!');
-});
+gateway.on('message', function message(data) {
+  let payload = JSON.parse(data.toString())
 
-client.on('interactionCreate', async interaction => {
-	const command = client.commands.get(interaction.commandName || "help");
+	switch (payload.op) {
+		case 10:
+			console.log("R 10 HELLO")
+			gateway.send(JSON.stringify({"op": 1,"d": 251}))
+			console.log("S 1 HB")
+			gateway.send(JSON.stringify({
+			  "op": 2,
+			  "d": {
+			    "token": token,
+			    "intents": 513,
+			    "properties": {
+			      "os": "linux",
+			      "browser": "funlib",
+			      "device": "funlib"
+			    }
+				}
+			}))
+			console.log("S 2 IDENT")
+			setInterval(() => {
+				gateway.send(JSON.stringify({"op": 1,"d": 251}))
+				console.log("S 1 HB")
+			}, payload.d.heartbeat_interval)
+			break;
 
-	if (!command) return;
+		case 0:
+			if (payload.t === "READY") console.log("R 0 EVENT/READY")
+			else if (payload.t === "INTERACTION_CREATE") interaction(payload)
+			else console.log("R 0 EVENT")
+			break;
 
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		case 11:
+			console.log("R 11 ACK")
+			break;
 	}
 });
 
-client.login(token)
+function interaction(payload) {
+	let commandName = payload.d.data.name
+	eval(`require("./commands/${commandName}.js").execute(payload.d)`)
+}
+
+// OLD CODE
+
+// const fs = require('node:fs');
+// const path = require('node:path');
+// const { Client, Collection, GatewayIntentBits } = require('discord.js');
+
+// const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// client.commands = new Collection();
+// const commandsPath = path.join(__dirname, 'commands');
+// const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+// for (const file of commandFiles) {
+// 	const filePath = path.join(commandsPath, file);
+// 	const command = require(filePath);
+// 	client.commands.set(command.name, command);
+// }
+
+// client.once('ready', () => {
+// 	console.log('Ready!');
+// });
+
+// client.on('interactionCreate', async interaction => {
+// 	const command = client.commands.get(interaction.commandName || "help");
+
+// 	if (!command) return;
+
+// 	try {
+// 		await command.execute(interaction);
+// 	} catch (error) {
+// 		console.error(error);
+// 		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+// 	}
+// });
+
+// client.login(token)
+
+
 
