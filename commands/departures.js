d38d22485fa48c750e2483ecf58948c8966abdcb
lyammosnier1 <64089164+iwl-lyam@users.noreply.github.com>
2022-08-16 12:38:24 +0000

    Fix codeshared flights

--- commands/departures.js
+++ commands/departures.js
@@ -19,33 +19,38 @@ module.exports = {
 			let f = {}
 			let embeds = []
 			let amount = interaction.options["amount"] || 5
+			let gates = []
 
-			if (!r) return api(`/webhooks/${appid}/${interaction.token}`, {body: {content: "The specified airport was not found."}})
+			if (!r[0]) return api(`/webhooks/${appid}/${interaction.token}`, {body: {content: "The specified airport was not found."}})
 			let actual = 1
 			
 			for (let i = 0; i < amount; i++) {
 				f = r[i]
-				if (!f) return api(`/webhooks/${appid}/${interaction.token}`, {body: {content: "The specified airport was not found."}})
-				console.log(f)
-				if (f['dep_time_ts'] || f['dep_time_ts'] || 0 < Math.floor(Date.now() / 1000)) {
+				if (!f) continue
+				if ((f['dep_time_ts'] || f['dep_time_ts'] || 0) < Math.floor(Date.now() / 1000)) {
 					amount++
 					continue
 				}
-				if (f['cs_flight_iata'] || (!f['airline_icao'] || f['flight_icao'])) {
+				if (f['cs_flight_iata'] || !(f['flight_icao'] || f['flight_iata'])) {
 					amount++
 					continue
 				}
+				if (gates.includes((f['dep_terminal'] || "")+(f['dep_gate'] || ""))) {
+					amount++
+					continue
+				}
+				gates.push((f['dep_terminal'] || "")+(f['dep_gate'] || ""))
 				let e = {
 					title: "Page " + actual,
 					fields: [
-						{ name: "Airline", value: f['airline_icao'] || "None", inline: true },
-						{ name: "Flight ICAO", value: f['flight_icao'] || "None", inline: true },
+						{ name: "Airline", value: f['airline_icao'] || f['airline_iata'] || "None", inline: true },
+						{ name: "Flight ICAO", value: f['flight_icao'] || f['flight_iata'] || "None", inline: true },
 						{ name: "Departure", value: `Terminal ${f['dep_terminal'] || "N/A"} (gate ${f['dep_gate'] || "N/A"})`, inline: true },
 						{ name: `Arrival at ${f['arr_iata'] || "N/A"} (${f['arr_icao']||"N/A"})` , value: `Terminal ${f['arr_terminal'] || "N/A"} (gate ${f['arr_gate'] || "N/A"})`, inline: true },
 						{ name: "Arrival baggage belt", value: f['arr_baggage'] || "Not decided", inline: true },
 						{ name: "ETD (Estimated Time of Departure)", value: f['dep_estimated_ts'] || f['dep_time_ts'] ? "<t:" + (f['dep_estimated_ts'] || f['dep_time_ts']) + ">" : "N/A", inline: true },
 						{ name: "ETA (Estimated Time of Arrival)", value: f['arr_estimated_ts'] || f['arr_time_ts'] ? "<t:" + (f['arr_estimated_ts'] || f['arr_time_ts']) + ">" : "N/A", inline: true },
-						{ name: "Delayed", value: f['delay'] || "N/A", inline: true },
+						{ name: "Delayed", value: (f['delayed'] || "N/A") + " minutes", inline: true },
 						{ name: "Status", value: f['status'] || "N/A", inline: true }
 					],
 					color: 5635925
